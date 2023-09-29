17f7293f82154b7353fc48b344290af55c71045b
bonface221 <mainaboniface221@gmail.com>
2023-09-30 00:44:14 +0300

    working with serializers

--- backend/tutorial/urls.py
+++ backend/tutorial/urls.py
@@ -10,7 +10,8 @@ admin.site.index_title = 'Admin'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('activate/', include('core.urls')),
-    # path('auth/jwt/create/',CustomTokenObtainPairView.as_view(),name='custom_jwt_create'),
+    path('auth/jwt/create/', CustomTokenObtainPairView.as_view(),
+         name='custom_jwt_create'),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
 ]
